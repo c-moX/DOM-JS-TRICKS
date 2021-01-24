5ab8275f98baeca738fb6cfcfae21a4f8beada3d
c-mox <cmoxsh@gmail.com>
2021-01-24 22:31:50 +0600

    DOM file reload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // Main JavaScript File
 
-var head = document.getElementById('header');
-head.style.color =  'red';
-head.style.fontSize = '50px';
+var authorBox = document.getElementsByTagName('article');
+for(var i = 0; i < authorBox.length; i++){
+    var element = authorBox[i];
+
+    element.style.border = '1px solid tomato';
+    element.style.borderRadius = '20px';
+    element.style.margin = '5px';
+    element.style.padding = '5px';
+}
+var authorContainer = document.getElementsByTagName('section');
+for(var i = 0; i < authorContainer.length; i++){
+    element2 = authorContainer[i];
+
+    element2.style.display = 'flex';
+    // element2.style.flexWrap = 'nowrap';
+}
+
 
